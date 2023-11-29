7a2dce6db913cf4faa20b9c1ac32ce3305e94cef
Jairo Quilumbaquin <jairoquilumbaquin1@gmail.com>
2023-11-28 20:35:26 -0500

    creacion del proyecto de API

--- Deber_6/WebApplication1/WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
+++ Deber_6/WebApplication1/WebApplication1/obj/Debug/net7.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ad2066ceae1bea8c717a85d00cb6ed4e84a1cc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
